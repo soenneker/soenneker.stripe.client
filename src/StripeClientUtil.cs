2123b4b3a0be8a72d894a05db0563ccae1eefe88
Jake Soenneker <jake@soenneker.com>
2026-01-05 15:07:48 -0600

    Static lamdba

--- src/StripeClientUtil.cs
+++ src/StripeClientUtil.cs
@@ -28,7 +28,7 @@ public sealed class StripeClientUtil : IStripeClientUtil
         {
             logger.LogDebug("Initializing Stripe client...");
 
-            HttpClient httpClient = await _httpClientCache.Get(nameof(StripeClientUtil), () => null, cancellationToken).NoSync();
+            HttpClient httpClient = await _httpClientCache.Get(nameof(StripeClientUtil), static () => null, cancellationToken).NoSync();
 
             var stripeClient = new SystemNetHttpClient(httpClient);
 
